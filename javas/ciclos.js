857d5f37bcdfddbfeb2e01638c9269226aec999d
jefier12 <jefierbriceno12@gmail.com>
2024-11-25 11:34:45 -0500

    estructuracion

--- javas/ciclos.js
+++ javas/ciclos.js
@@ -11,20 +11,68 @@
 //     {
 //         console.log(i * 2);
 //     }
-let primos = 0;
-let i = 1;
-while(primos < 10)
-{    
-    let cont=0
-    for(let x = 1; x <= i; x++)
-    {
-        ix = i % x
-        if(ix === 0) cont++;
-    }
-    if(cont === 2) 
-    {
-        primos++;
-        console.log(`${primos}.${i}`);    
-    }
-    i++;
+// let primos = 0;
+// let i = 1;
+// while(primos < 10)
+// {    
+//     let cont=0
+//     for(let x = 1; x <= i; x++)
+//     {
+//         ix = i % x
+//         if(ix === 0) cont++;
+//     }
+//     if(cont === 2) 
+//     {
+//         primos++;
+//         console.log(`${primos}.${i}`);    
+//     }
+//     i++;
+// }
+
+
+// let numero = parseFloat(prompt(" ingrese el numero a evaluar: "))
+// const esPrimo = (a) => {
+//     try{
+//         let primo = true;
+//         if (a ==== 1) return false;
+//         for (let i = 2; i =)
+//     }
+    
+
+// }
+
+
+// for (let i = 0; 1 < 10 ; i++){
+//     if (i === 4) continue;
+//     console.log(`Item ${i}`);
+// }
+
+// const numeros = [1,2,3,4];
+
+// // let a = numeros[0];
+// // let b = numeros[1];
+// // let c = numeros[2];
+// // let d = numeros[3];
+
+// let [a,b,c,d,e = 25] = numeros;
+
+// console.log(a,b,c,d,e);
+
+
+
+
+const persona = {
+    edad : 17,
+    nombre : "jefier stevan briceño vera",
+    username : "pepe2"
 }
+
+const { edad:edad2,nombre:nombre2,username="pepe" } = persona;
+
+console.log(edad2,nombre2,username)
+
+
+// let caracter = persona.edad;
+// let caracter2 = persona.nombre;
+
+// console.log(persona);
